c8904a33354ba16a116a6e83abeee5b4a5c21337
patrykfussek <patryk.fussek@gmail.com>
2023-03-06 14:59:16 +0100

    Beta version of Firebase backend integration

--- src/components/FinalBoard.js
+++ src/components/FinalBoard.js
@@ -1,10 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import '../styles/final_board_styles.css';
 import RandomPin from './RandomPin.js';
 import Pin from './Pin.js';
 import Modal from './Modal.js';
 import OpenPin from './OpenPin.js';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { firestore } from '../firebase_setup/firebase.js';
+
+// function FinalBoard() {
+//   var thePin = {};
+//   const [boardDetails, setBoardDetails] = useState({
+//     pins: [],
+//     show_modal: false,
+//     show_open_pin: false,
+//   });
+
+//   const openPin = (pinDetails) => {
+//     thePin = { ...pinDetails };
+//     setBoardDetails((boardDetails) => {
+//       return {
+//         ...boardDetails,
+//         show_modal: false,
+//         show_open_pin: true,
+//       };
+//     });
+//     //todo: set the open pin modal to center of the screen (when scrolled down the page)
+//   };
+
+//   const shufflePins = () => {
+//     let newPin = RandomPin();
+//     addPin(newPin);
+//   };
+
+//   const addPin = (pinDetails) => {
+//     setBoardDetails((boardDetails) => {
+//       const new_pins = [...boardDetails.pins];
+//       new_pins.push(<Pin pinDetails={pinDetails} key={boardDetails.pins.length} openPin={openPin} />);
+//       // openPin={openPin}
+//       return {
+//         pins: new_pins,
+//         show_modal: false,
+//         show_open_pin: false,
+//       };
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <div className='navigation_bar'>
+//         <div onClick={() => setBoardDetails({ ...boardDetails, show_modal: true })} className='pint_mock_icon_container add_pin'>
+//           <img src='./images/add.png' alt='add_pin' className='pint_mock_icon' />
+//         </div>
+//         <div className='pint_mock_icon_container add_pin'>
+//           <img src='./images/ellipse.png' alt='menu' className='pint_mock_icon' />
+//         </div>
+//         <div onClick={() => shufflePins()} className='pint_mock_icon_container add_pin'>
+//           <img src='./images/shuffle.png' alt='shuffle' className='pint_mock_icon' />
+//         </div>
+//       </div>
+//       <div className='pin_container'>{boardDetails.pins}</div>
+//       <div onClick={(event) => (event.target.className === 'add_pin_modal' ? setBoardDetails({ ...boardDetails, show_modal: false }) : null)} className='add_pin_modal_container'>
+//         {boardDetails.show_modal ? <Modal addPin={addPin} /> : null}
+//       </div>
+//       <div onClick={(event) => (event.target.className === 'open_pin_modal' ? setBoardDetails({ ...boardDetails, show_open_pin: false }) : null)} className='open_pin_modal_container'>
+//         {boardDetails.show_open_pin ? <OpenPin pinDetails={thePin} /> : null}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default FinalBoard;
 
 class FinalBoard extends React.Component {
   constructor(props) {
@@ -17,17 +83,45 @@ class FinalBoard extends React.Component {
     };
   }
 
-  addPin = (pinDetails) => {
-    this.setState((_state) => {
-      const new_pins = [..._state.pins];
-      new_pins.push(<Pin pinDetails={pinDetails} key={_state.pins.length} openPin={this.openPin} />);
+  componentDidMount() {
+    this.fetchPost();
+  }
 
-      return {
-        pins: new_pins,
-        show_modal: false,
-        show_open_pin: false,
-      };
+  //todo: https://ordinarycoders.com/blog/article/reactjs-best-practices
+  // use componentDidMount() lifecycle method here
+
+  fetchPost = async () => {
+    this.setState({ is_loading: true });
+    await getDocs(collection(firestore, 'pins')).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      let fetchedPins = [];
+      newData.forEach((p) => {
+        fetchedPins.push(<Pin pinDetails={p} key={p.id} openPin={this.openPin} />);
+      });
+      this.setState((_state) => {
+        return {
+          pins: fetchedPins,
+          show_modal: false,
+          show_open_pin: false,
+        };
+      });
+      // console.log(this.state.pins, newData);
+      this.setState({ is_loading: false });
     });
+  };
+
+  addPin = async (pinDetails) => {
+    //todo: idea --> change addPin to refreshPins, or fetchPins, as the plan is
+    await this.fetchPost();
+    // this.setState((_state) => {
+    //   const new_pins = [..._state.pins];
+    //   new_pins.push(<Pin pinDetails={pinDetails} key={_state.pins.length} openPin={this.openPin} />);
+    //   return {
+    //     pins: new_pins,
+    //     show_modal: false,
+    //     show_open_pin: false,
+    //   };
+    // });
   };
 
   openPin = (pinDetails) => {
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
-
+import LoadingIcon from './LoadingIcon';
+// import handleSubmit from '../firebase_setup/handleSubmit.js';
+import { collection, addDoc, getDoc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { firestore } from '../firebase_setup/firebase.js';
 import '../styles/modal_styles.css';
+let imageFile;
 
 function uploadImage(event, pinDetails, setPinDetails, setShowLabel, setShowModalPin) {
   if (event.target.files && event.target.files[0]) {
     if (/image\/*/.test(event.target.files[0].type)) {
       const reader = new FileReader();
-
+      reader.readAsDataURL(event.target.files[0]);
       reader.onload = function () {
         setPinDetails({
           ...pinDetails,
-          img_blob: reader.result,
+          img_url: reader.result,
         });
         setShowLabel(false);
         setShowModalPin(true);
       };
-
-      reader.readAsDataURL(event.target.files[0]);
+      imageFile = event.target.files[0];
     }
   }
 }
@@ -30,7 +34,8 @@ function checkSize(event) {
   image.style.opacity = 1;
 }
 
-function savePin(pinDetails, addPin) {
+async function savePin(setIsLoading, e, pinDetails, addPin) {
+  setIsLoading(true);
   const users_data = {
     ...pinDetails,
     author: 'Patryk',
@@ -40,7 +45,51 @@ function savePin(pinDetails, addPin) {
     destination: document.querySelector('#pin_destination').value,
     pin_size: document.querySelector('#pin_size').value,
   };
+  //todo: figure out saving to Firebase
+  let doc = await savePinBackend(e, users_data);
   addPin(users_data);
+  setIsLoading(false);
+}
+
+//todo: extract to external function, optimize, minimize API calls...
+async function savePinBackend(e, users_data) {
+  let docSnap;
+  e.preventDefault();
+  try {
+    const docRef = await addDoc(collection(firestore, 'pins'), {
+      ...users_data,
+      img_url: '',
+    });
+    const storage = getStorage();
+    const storageRef = ref(storage, docRef.id);
+    await uploadBytes(storageRef, imageFile)
+      .then((snapshot) => {
+        console.log('Uploaded image for pin: ' + docRef.id);
+        getDownloadURL(snapshot.ref)
+          .then((url) => {
+            updateDoc(docRef, { img_url: url })
+              .then((docRef) => {
+                console.log('Update of pin sucessful!');
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log('Document written with ID: ', docRef.id);
+    docSnap = await getDoc(docRef);
+
+    console.log(docSnap.data());
+    return docSnap.data();
+  } catch (e) {
+    console.error('Error adding document: ', e);
+  }
 }
 
 function Modal(props) {
@@ -50,15 +99,17 @@ function Modal(props) {
     title: '',
     destination: '',
     description: '',
-    img_blob: '',
+    img_url: '',
     pin_size: '',
   });
   const [showLabel, setShowLabel] = useState(true);
   const [showModalPin, setShowModalPin] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <div className='add_pin_modal'>
       <div className='add_pin_container'>
+        {isLoading ? <LoadingIcon /> : null}
         <div className='side' id='left_side'>
           <div className='section1'>
             <div className='pint_mock_icon_container'>
@@ -80,7 +131,7 @@ function Modal(props) {
             </label>
             <div className='modals_pin' style={{ display: showModalPin ? 'block' : 'none' }}>
               <div className='pin_image'>
-                <img onLoad={checkSize} src={pinDetails.img_blob} alt='pin_image' />
+                <img onLoad={checkSize} src={pinDetails.img_url} alt='pin_image' />
               </div>
             </div>
           </div>
@@ -98,7 +149,7 @@ function Modal(props) {
                 <option value='medium'>Medium</option>
                 <option value='large'>Large</option>
               </select>
-              <div onClick={() => savePin(pinDetails, props.addPin)} className='save_pin'>
+              <div onClick={(e) => savePin(setIsLoading, e, pinDetails, props.addPin)} className='save_pin'>
                 Save
               </div>
             </div>
