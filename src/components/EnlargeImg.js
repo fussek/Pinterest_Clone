6731dd5f17aade0111bbcf53753bf3fc74b6ccae
patrykfussek <patryk.fussek@gmail.com>
2023-03-18 23:22:23 +0100

    Enlarge Image mod + animation

--- src/components/EnlargeImg.js
+++ src/components/EnlargeImg.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import '../styles/enlarge_img_styles.css';
 
+function handleClick(props) {
+  props.setShowLargeImg(!props.showLargeImg);
+}
+
 function EnlargeImg(props) {
-  return <img onClick={() => props.setShowLargeImg(!props.showLargeImg)} className='image' src={props.src} alt='img' />;
+  return (
+    <div className='background'>
+      <img onClick={() => handleClick(props)} className='image' src={props.src} alt='img' />
+    </div>
+  );
 }
 
 export default EnlargeImg;
