64fa3a9332797c7dd8df8b3f5b1634f7eb413e29
thebambooguy <szel.patryk@gmail.com>
2023-05-04 10:42:52 +0200

    adding utils and updating gitignore

---
+++ python/utils.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def configure_logging():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
